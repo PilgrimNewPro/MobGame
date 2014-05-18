ece2e9b95fda345d91c9e1c5f8346d01d713c240
pilgrimroad <79827195@qq.com>
2014-05-18 14:05:57 +0800

    创建项目。

---
+++ Classes/Common/Type.h
@@ -0,0 +1,28 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define VOID void
+typedef char			CHAR;
+typedef unsigned char	BYTE;
+typedef int				BOOL;
+typedef short			SHORT;
+typedef unsigned short	USHORT;
+typedef int				INT;
+typedef unsigned int	UINT;
+typedef float			FLOAT;
+typedef long			LONG;
+typedef unsigned long	ULONG;
+typedef double			DOUBLE;
+
+typedef long long		INT64;
+typedef unsigned long long UINT64;
+
+typedef std::string		STRING;
+
+typedef USHORT			PacketID_t;
+typedef ULONG			IP_t;
+
+typedef INT				TaskID_t;
+typedef INT				FactionID_t;
+
+#endif // __COMMON_H__
